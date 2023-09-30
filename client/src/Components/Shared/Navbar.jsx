d82faa0ce1fd7f12b5bb82c75bad8e1c065db28f
toukir15 <toukir.developer.bd@gmail.com>
2023-09-30 10:37:44 +0600

    complete navbar

--- client/src/Components/Shared/Navbar.jsx
+++ client/src/Components/Shared/Navbar.jsx
@@ -1,9 +1,59 @@
+import { AiOutlineMessage, AiOutlineSearch } from "react-icons/ai";
+import { BsBell } from "react-icons/bs";
+
 export default function Navbar() {
   return (
-    <div className="flex justify-between px-8 bg-white shadow-sm py-6 rounded-lg ">
-      <div>menu</div>
-      <div>search</div>
-      <div>profile</div>
+    <div className="flex items-center px-8 bg-[#081229] text-white shadow-sm py-6 rounded-lg ">
+      <div className="text-lg font-medium w-1/3 ">Dashboard</div>
+      <div className="w-1/3 px-8 relative">
+        <input
+          type="text"
+          placeholder="Search here..."
+          className="border-2 border-[#8633FF] outline-[#8633FF] px-4 py-2 w-full rounded-3xl "
+        />
+        <button className="w-8 h-8 mt-[1px] bg-[#8633FF] hover:bg-[#812dff] transition-all duration-100 shadow-xl text-white rounded-full flex justify-center items-center absolute top-1 right-10">
+          <AiOutlineSearch />
+        </button>
+      </div>
+      <div className="w-1/3 flex justify-center items-center gap-4 ">
+        <div className="bg-gray-800 p-3 rounded relative">
+          <BsBell size={24} />
+          <span className="animate-ping absolute top-1 right-1 inline-flex h-3 w-3 rounded-full bg-red-400 opacity-75"></span>
+          <span className="h-1 w-1 rounded-full bg-red-500 absolute inline top-2 right-2"></span>
+        </div>
+        <div className="bg-gray-800 p-3 rounded relative">
+          <AiOutlineMessage size={24} />
+          <span className="animate-ping absolute top-1 right-1 inline-flex h-3 w-3 rounded-full bg-red-400 opacity-75"></span>
+          <span className="h-1 w-1 rounded-full bg-red-500 absolute inline top-2 right-2"></span>
+        </div>
+        <div className="dropdown dropdown-end">
+          <label
+            tabIndex={0}
+            className="btn btn-ghost btn-circle avatar w-12 border-2 border-[#8633FF]"
+          >
+            <div className="w-10 rounded-full">
+              <img src="https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=ahypUC_KTc95VOsBkzLFZiCQ0VJwewfrSV43BOrLETM=" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 text-black"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/Components/Shared/Sidebar.jsx
+++ client/src/Components/Shared/Sidebar.jsx
@@ -13,9 +13,9 @@ export default function Sidebar() {
   const { isSidebarOpen, setIsSidebarOpen } = useContext(GlobalContext);
   return (
     <div
-      className={`sticky bottom-0 top-0 pt-8 ${
-        isSidebarOpen ? "pl-4" : ""
-      }  bg-white h-screen`}
+      className={`sticky bottom-0 top-0 pt-8 bg-[#081229] ${
+        isSidebarOpen ? "px-8" : " px-6"
+      }  h-screen hidden md:block`}
     >
       <div>
         <div
@@ -23,17 +23,17 @@ export default function Sidebar() {
           style={{
             boxShadow: "0px 0px 10px -1px rgba(0, 0, 0, 0.1)",
           }}
-          className={`border  w-8 h-8 rounded-full ${
+          className={`border  w-8 h-8 rounded-full transition-all duration-500 ${
             isSidebarOpen ? "-rotate-90" : "rotate-90"
           }  flex justify-center items-center absolute top-5 right-1 bg-white shadow-lg translate-x-1/2 cursor-pointer hover:shadow-none border hover:border-[#8633FF]`}
         >
           <MdOutlineExpandLess size={24} />
         </div>
-        <div className=" flex flex-col justify-between w-full h-[600px] mx-auto items-center mt-20">
+        <div className=" flex flex-col justify-between w-full h-[calc(100vh-100px)] mx-auto items-center mt-10 relative">
           {/* top part of the slider  */}
-          <div className="space-y-2">
+          <div className="space-y-2 ">
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
@@ -41,7 +41,7 @@ export default function Sidebar() {
               {isSidebarOpen && <p>Dashboard</p>}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
@@ -49,7 +49,7 @@ export default function Sidebar() {
               {isSidebarOpen && <p>Management</p>}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
@@ -57,7 +57,7 @@ export default function Sidebar() {
               {isSidebarOpen && <p>All stores</p>}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
@@ -65,7 +65,7 @@ export default function Sidebar() {
               {isSidebarOpen && <p>Add store</p>}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
@@ -73,34 +73,41 @@ export default function Sidebar() {
               {isSidebarOpen && <p>Profit tracker</p>}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
               <GoChecklist size={20} />
-              {isSidebarOpen && <p>Pending Arrival Form </p>}
+              {isSidebarOpen && (
+                <p className="whitespace-nowrap">Pending Arrival Form </p>
+              )}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
               <GoChecklist size={20} />
-              {isSidebarOpen && <p>Pending Request Form </p>}
+              {isSidebarOpen && (
+                <p className="whitespace-nowrap">Pending Request Form </p>
+              )}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
               <GoChecklist size={20} />
-              {isSidebarOpen && <p>Add ASIN/UPC Form </p>}
+              {isSidebarOpen && (
+                <p className="whitespace-nowrap">Add ASIN/UPC Form </p>
+              )}
             </div>
           </div>
+
           {/* bottom part of the slider  */}
-          <div className="space-y-2">
+          <div className="space-y-2 ">
             <div
-              className={`bg-[#8633FF] text-white ps-2 ${
+              className={`bg-[#8633FF] text-white  ps-2 ${
                 isSidebarOpen
                   ? "pe-[119px] py-[6px]"
                   : "px-3 py-2 flex justify-center items-center"
@@ -110,7 +117,7 @@ export default function Sidebar() {
               {isSidebarOpen && <p>Support</p>}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
@@ -118,7 +125,7 @@ export default function Sidebar() {
               {isSidebarOpen && <p>Settings</p>}
             </div>
             <div
-              className={`bg-[#8633FF] text-white ${
+              className={`bg-[#8633FF] text-white  ${
                 isSidebarOpen ? "ps-2 pe-4 py-[6px]" : "px-3 py-2"
               }   rounded-md flex items-center gap-2`}
             >
