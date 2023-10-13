0255641f15f54897169026ed07353cd574cb87dc
torikul Islam <toriqulislam142@gmail.com>
2023-10-14 00:31:54 +0600

    update

--- server/src/middlewares/verifyJWT.js
+++ server/src/middlewares/verifyJWT.js
@@ -1,19 +1,22 @@
 const jwt = require("jsonwebtoken")
 const verifyJWT = (req, res, next) => {
     const { authorization } = req.headers;
+
     try {
         const token = authorization.split(' ')[1]
-        if (!token) {
+        if (token === 'undefined') {
             return res.json({ message: "Unauthorized access" })
         }
-        const decode = jwt.verify(token, process.env.JWT_SECRET)
-        const { role, id, email } = decode
-        req.role = role,
-            req.id = id,
-            req.email = email
-        next()
+        else {
+            const decode = jwt.verify(token, process.env.JWT_SECRET)
+            const { role, id, email } = decode
+            req.role = role,
+                req.id = id,
+                req.email = email
+            next()
+        }
     } catch (error) {
-        next("Check login error")
+        next(error)
     }
 }
 
