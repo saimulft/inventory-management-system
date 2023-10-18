ad2befb2d2a03f141ffac8de3bb1ec8dbb2fbee8
torikul Islam <toriqulislam142@gmail.com>
2023-10-18 21:43:27 +0600

    update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const cors = require("cors")
 
-
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true,limit:'5mb'}))
