f2075957e1f736025151870108bc3cfec7a37b09
torikul Islam <toriqulislam142@gmail.com>
2023-11-06 21:32:09 +0600

    update store details

--- client/src/Pages/StoreEditPage.jsx
+++ client/src/Pages/StoreEditPage.jsx
@@ -6,15 +6,24 @@ import { BiSolidEdit } from "react-icons/bi";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import useGlobal from "../hooks/useGlobal";
+import useAuth from "../hooks/useAuth";
+import ToastMessage from "../Components/Shared/ToastMessage";
+import { FaSpinner } from "react-icons/fa";
 
 export default function StoreEditPage() {
   const [addSupplier, setAddSupplier] = useState([{ id: 1 }]);
   const [supplierInfoInputList, setSupplierInfoInputList] = useState([])
   const [additionalPaymentInputList, setAdditionalPaymentInputList] = useState([])
+  const { isSidebarOpen } = useGlobal()
+  const [successMessage, setSuccessMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const { user } = useAuth()
+  const [loading, setLoading] = useState(false)
 
-  const {id} = useParams()
+  const { id } = useParams()
 
-  const { data: singleStore = [] } = useQuery({
+  const { data: singleStore = [], refetch } = useQuery({
     queryKey: ['single_store'],
     queryFn: async () => {
       try {
@@ -44,7 +53,53 @@ export default function StoreEditPage() {
     }
     setAddSupplier(list);
   };
-
+  const handleUpdateModal = () => {
+    document.getElementById("update_modal").showModal()
+  }
+  const handleUpdate = (e) => {
+    e.preventDefault()
+    setErrorMessage("")
+    setSuccessMessage("")
+    const form = e.target
+    const storeName = form.storeName.value
+    const storeManagername = form.storeManagername.value
+    const storeStatus = form.storeStatus.value
+    const storeType = form.storeType.value
+    if (!storeName && !storeManagername && storeStatus === "Select Status" && storeType === "Pick Store Type") {
+      return;
+    }
+    const updateData = {
+      storeName,
+      storeManagername,
+      storeStatus,
+      storeType
+    }
+    setLoading(true)
+    axios.post(`/api/v1/store_api//update_store_details?id=${user.admin_id}`, updateData)
+      .then(res => {
+     
+        if (res.status === 200) {
+          setLoading(false)
+          refetch()
+          form.reset()
+          setSuccessMessage("Updated")
+          setTimeout(() => {
+            setSuccessMessage("")
+          }, 2000);
+        }
+        if (res.status === 204) {
+          setLoading(false)    
+          setErrorMessage("Up to date")
+          setTimeout(() => {
+            setErrorMessage("")
+          }, 2000);
+        }
+      }).catch(err => {
+        setLoading(false)
+        console.log(err)
+      })
+  }
+  const marginLeft = isSidebarOpen ? "18.5%" : "6%";
   return (
     <div className="p-10 bg-white rounded-lg">
       {/* option select  */}
@@ -71,7 +126,7 @@ export default function StoreEditPage() {
               <span className="font-bold text-slate-600">Store status: </span>
               <span>{singleStore.store_status}</span>
             </p>
-            <button className="border border-[#8633FF] px-4 py-1 flex justify-center items-center gap-2 my-4 rounded hover:bg-[#8633FF] hover:text-white transition-all">
+            <button onClick={handleUpdateModal} className="border border-[#8633FF] px-4 py-1 flex justify-center items-center gap-2 my-4 rounded hover:bg-[#8633FF] hover:text-white transition-all">
               <BiSolidEdit />
               <p>Edit</p>
             </button>
@@ -122,6 +177,63 @@ export default function StoreEditPage() {
           </div>
         );
       })}
+      <dialog id="update_modal" className="modal">
+        <div style={{ marginLeft, maxWidth: '500px' }} className="modal-box py-10 px-10">
+          <form onSubmit={handleUpdate} className="flex justify-center flex-col">
+            <label className="text-slate-500 font-bold mb-1">New store name</label>
+            <input
+              className="border bg-white shadow-md border-[#8633FF] outline-none w-full py-3 rounded-md px-2 text-sm"
+              placeholder="Store name"
+              name="storeName"
+              type="text"
+            />
+            <label className=" text-slate-500 mt-4 font-bold mb-1">New store manager name</label>
+            <input
+              className="border bg-white shadow-md border-[#8633FF] outline-none w-full py-3 rounded-md px-2 text-sm"
+              placeholder="Store name"
+              name="storeManagername"
+              type="text"
+            />
+            <label className="font-bold text-slate-500 mt-4">Store type*</label>
+            <select
+              className="select select-primary w-full mt-2"
+              name="storeType"
+              id="storeType"
+              required
+            >
+              <option defaultValue="Pick Store Type">
+                Pick Store Type
+              </option>
+              <option value="Amazon">Amazon</option>
+              <option value="Walmart">Walmart</option>
+              <option value="Ebay">Ebay</option>
+              <option value="Shopify">Shopify</option>
+              <option value="Ali Express">Ali Express</option>
+            </select>
+            <label className="font-bold text-slate-500 mt-4">Store status*</label>
+            <select
+              className="select select-primary w-full mt-2"
+              name="storeStatus"
+              id="storeStatus"
+              required
+            >
+              <option defaultValue="Select Status">
+                Select Status
+              </option>
+              <option value="Active">Active</option>
+              <option value="Inactive">Inactive</option>
+            </select>
+            {successMessage && <ToastMessage errorMessage={errorMessage} successMessage={successMessage} />}
+            <button type="submit" disabled={loading} className="bg-[#8633FF] mt-4 flex gap-2 py-2 justify-center items-center text-white rounded-lg w-full">
+              {loading && <FaSpinner size={20} className="animate-spin" />}
+              Update
+            </button>
+          </form>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
   );
 }
