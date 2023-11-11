3a1babac4660bd5245250c14fd3cbcf1d86fdef0
NabilNewaz <nabilnewaz@gmail.com>
2023-11-11 13:54:29 +0600

    removed old

--- client/src/Pages/SettingPages/BillingAndSubscriptionPage.jsx
+++
@@ -1,9 +0,0 @@
-import BillingAndSubscription from "../../Components/SettingsPageComponents/BillingAndSubscription.jsx/BillingAndSubscription";
-
-export default function BillingAndSubscriptionPage() {
-  return (
-    <div>
-      <BillingAndSubscription />
-    </div>
-  );
-}
--- client/src/hooks/useGlobal.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { GlobalContext } from '../Providers/GlobalProviders';
-
-const useGlobal = () => {
-    const globalInfo = useContext(GlobalContext)
-    return globalInfo;
-};
-
-export default useGlobal;
