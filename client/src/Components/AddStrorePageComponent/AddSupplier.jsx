f8bdeedb404d81acff24f6f186009fbf91508b76
NabilNewaz <nabilnewaz@gmail.com>
2023-11-15 19:09:19 +0600

    ad store updated

--- client/src/Components/AddStrorePageComponent/AddSupplier.jsx
+++ client/src/Components/AddStrorePageComponent/AddSupplier.jsx
@@ -9,11 +9,16 @@ import useAuth from "../../hooks/useAuth";
 import { MdErrorOutline } from "react-icons/md";
 
 export default function AddSupplier() {
-  const [addSupplier, setAddSupplier] = useState([{ id: 1 }]);
-  const { storeDetails, setStoreDetails, supplierInfoInputList, setSupplierInfoInputList, additionalPaymentInputList, setAdditionalPaymentInputList } = useStore()
+  const { storeDetails, setStoreDetails } = useStore()
+
+  const [addSupplier, setAddSupplier] = useState(storeDetails?.supplier_information?.length > 0 ? parseInt(storeDetails?.supplier_information?.length) : 1);
+
+  const [isValidCardNumber, setIsValidCardNumber] = useState([])
+  const [isValidCardExp, setIsValidCardExp] = useState([])
+  const [isValidCardCvv, setIsValidCardCvv] = useState([])
+
   const { user } = useAuth()
-  const [supplierInputError, setSupplierInputError] = useState('')
-  const [paymentInputError, setPaymentInputError] = useState('')
+  const [inputError, setInputError] = useState([])
 
   const navigate = useNavigate()
 
@@ -21,74 +26,105 @@ export default function AddSupplier() {
     return <Navigate to="/dashboard/add-store" />
   }
 
-  const handleAddSupplierIncrementField = () => {
-    setAddSupplier([...addSupplier, {}]);
-  };
-
-  const handleAddSupplierRemoveField = (index) => {
-    const list = [...addSupplier];
-
-    if (index > 0 && index < list.length) {
-      list.splice(index, 1);
-    }
-    setAddSupplier(list);
-  };
-
   const handleNext = async () => {
-    const date = new Date().toISOString();
+    const suppliers_data = [];
+    let additional_payment_data = [];
+    const error_data = [];
 
-    let isNext;
+    for (let i = 0; i < document.getElementById('store_parent_div').children.length; i++) {
+      let supplier_list = [];
+      for (let j = 0; j < document.getElementById('store_parent_div').children[i].children[0].children[0].children[1].children.length; j++) {
 
-    supplierInfoInputList.forEach(singleList => {
-      setSupplierInputError('')
-      isNext = undefined;
+        const name = document.getElementById('store_parent_div').children[i].children[0].children[0].children[1].children[j].children[0].value;
+        const username = document.getElementById('store_parent_div').children[i].children[0].children[0].children[1].children[j].children[1].value;
+        const password = document.getElementById('store_parent_div').children[i].children[0].children[0].children[1].children[j].children[2].value;
 
-      if (!singleList.supplier_name || !singleList.username || !singleList.password) {
-        return setSupplierInputError('Missing supplier information')
-      }
-
-      return isNext = true;
-    })
-
-    if (!isNext) {
-      return;
-    }
-
-    additionalPaymentInputList.forEach(singleList => {
-      setPaymentInputError('')
-      isNext = undefined;
-
-      if (additionalPaymentInputList.length > 1) {
-        if (!singleList.email || !singleList.card_name || !singleList.card_info || !singleList.date || !singleList.cvc || !singleList.billing_address || !singleList.city || !singleList.state || !singleList.zip_code || !singleList.country) {
-          return setPaymentInputError('Missing additional payment details')
+        if (name && username && password) {
+          if (error_data.filter((element) => element.type == 'supplier' && element.id == document.getElementById('store_parent_div').children[i].id).length > 0) {
+            error_data.splice(error_data.findIndex(a => a.type == 'supplier' && a.id == document.getElementById('store_parent_div').children[i].id), 1)
+          }
+          supplier_list.push({
+            parent_id: i + 1,
+            id: j + 1,
+            name: name,
+            username: username,
+            password: password,
+          })
+        } else {
+          error_data.push({ type: 'supplier', id: document.getElementById('store_parent_div').children[i].id })
         }
       }
-      else {
-        if (singleList.email || singleList.card_name || singleList.card_info || singleList.date || singleList.cvc || singleList.billing_address || singleList.city || singleList.state || singleList.zip_code || singleList.country && singleList.country !== 'Select country') {
+      if (supplier_list.length > 0) {
+        suppliers_data.push(supplier_list)
+      }
+    }
 
-          if (!singleList.email || !singleList.card_name || !singleList.card_info || !singleList.date || !singleList.cvc || !singleList.billing_address || !singleList.city || !singleList.state || !singleList.zip_code || !singleList.country) {
-            return setPaymentInputError('Missing additional payment details')
+    for (let i = 0; i < document.getElementById('store_parent_div').children.length; i++) {
+      let additional_payment_list = [];
+      for (let j = 0; j < document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children.length; j++) {
+
+        const email = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[0].children[1].value;
+        const name_on_card = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[1].children[1].value;
+        const card_number = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[2].children[1].value;
+        const card_date = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[3].children[0].children[0].value;
+        const card_cvc = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[3].children[1].children[0].value;
+        const billing_address = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[4].children[0].children[1].value;
+        const city = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[4].children[1].children[1].value;
+        const state = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[5].children[0].children[1].value;
+        const zip_code = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[5].children[1].children[1].value;
+        const country = document.getElementById('store_parent_div').children[i].children[0].children[1].children[1].children[j].children[0].children[0].children[2].children[0].children[6].children[1].value;
+
+        if (email || name_on_card || card_number || card_date || card_cvc || billing_address || city || state || zip_code || country != 'Select country') {
+          if (email && name_on_card && card_number && card_date && card_cvc && billing_address && city && state && zip_code && country) {
+            if (error_data.filter((element) => element.type == 'payment' && element.id == document.getElementById('store_parent_div').children[i].id).length > 0) {
+              error_data.splice(error_data.findIndex(a => a.type == 'payment' && a.id == document.getElementById('store_parent_div').children[i].id), 1)
+            }
+            additional_payment_list.push({
+              parent_id: i + 1,
+              id: j + 1,
+              email: email,
+              name_on_card: name_on_card,
+              card_info: {
+                number: card_number,
+                date: card_date,
+                cvc: card_cvc,
+              },
+              billing_address: billing_address,
+              city: city,
+              state: state,
+              zip_code: zip_code,
+              country: country
+            })
+          } else {
+            error_data.push({ type: 'payment', id: document.getElementById('store_parent_div').children[i].id })
           }
         }
       }
-
-      return isNext = true;
-    })
-
-    if (!isNext) {
-      return;
+      if (additional_payment_list.length > 0) {
+        additional_payment_data.push(additional_payment_list)
+      }
     }
 
-    setStoreDetails({
-      ...storeDetails,
-      supplier_information: supplierInfoInputList,
-      additional_payment_details: additionalPaymentInputList.length > 1 ? additionalPaymentInputList : additionalPaymentInputList.length === 1 && additionalPaymentInputList[0].email ? additionalPaymentInputList : [],
-      admin_id: user.admin_id,
-      date: date,
-      creator_email: user?.email,
-    })
+    if (error_data.length <= 0 && isValidCardCvv.length <= 0 && isValidCardExp.length <= 0 && isValidCardNumber.length <= 0) {
+      const date = new Date().toISOString();
+      setStoreDetails({
+        ...storeDetails,
+        supplier_information: suppliers_data,
+        additional_payment_details: additional_payment_data,
+        admin_id: user.admin_id,
+        date: date,
+        creator_email: user?.email,
+      })
+      navigate("/dashboard/add-store/add-supplier/select-payment")
+    }
 
-    navigate("/dashboard/add-store/add-supplier/select-payment")
+    setInputError(error_data)
+  }
+
+  const handleAddSupplierRemoveField = (index) => {
+    if (document.getElementById(`store_parent_div`).children.length > 1) {
+      document.getElementById(`supplier_parent_div_${index}`).remove();
+    }
   }
 
   return (
@@ -122,51 +158,54 @@ export default function AddSupplier() {
       </div>
 
       {/* add information  */}
-      {addSupplier.map((a, index) => {
-        return (
-          <div key={index} className="relative z-0">
-            <div className="mt-8 border-2 border-[#8633FF] flex rounded-lg ">
-              {/* supplier information  */}
-              <div className="w-1/2 p-8">
-                <div>
-                  <h5 className="text-xl font-medium">Add Supplier Information {index}</h5>
-                  {supplierInputError && <p className="w-full mt-3 flex gap-1 items-center justify-center text-center text-sm font-medium text-rose-600 bg-rose-100 border py-2 px-4 rounded"><MdErrorOutline size={20} /> {supplierInputError}</p>}
+      <div id="store_parent_div">
+        {Array(addSupplier).fill().map((a, index) => {
+          return (
+            <div id={`supplier_parent_div_${index}`} key={index} className="relative z-0">
+              <div className="mt-8 border-2 border-[#8633FF] flex rounded-lg ">
+                {/* supplier information  */}
+                <div className="w-1/2 p-8">
+                  <div>
+                    <h5 className="text-xl font-medium">Add Supplier Information</h5>
+                    {(inputError.filter((element) => element.id == `supplier_parent_div_${index}` && element.type == 'supplier').length > 0) && <p className="w-full mt-3 flex gap-1 items-center justify-center text-center text-sm font-medium text-rose-600 bg-rose-100 border py-2 px-4 rounded"><MdErrorOutline size={20} /> {'Missing supplier information'}</p>}
+                  </div>
+
+                  <SupplierInfoInputList from_index={index + 1} />
                 </div>
 
-                <SupplierInfoInputList/>
+                {/* add payment details  */}
+                <div className="w-1/2 p-4 pb-10">
+                  <div>
+                    {(inputError.filter((element) => element.id == `supplier_parent_div_${index}` && element.type == 'payment').length > 0) && <p className="w-full mt-3 flex gap-1 items-center justify-center text-center text-sm font-medium text-rose-600 bg-rose-100 border py-2 px-4 rounded"><MdErrorOutline size={20} /> {'Missing additional payment details'}</p>}
+                  </div>
+                  <AdditionalPaymentInputList from_index={index + 1} isValidCardNumber={isValidCardNumber} setIsValidCardNumber={setIsValidCardNumber} isValidCardExp={isValidCardExp} setIsValidCardExp={setIsValidCardExp} isValidCardCvv={isValidCardCvv} setIsValidCardCvv={setIsValidCardCvv} />
+                </div>
               </div>
 
-              {/* add payment details  */}
-              <div className="w-1/2 p-4 pb-10">
-                {paymentInputError && <p className="w-full mt-3 flex gap-1 items-center justify-center text-center text-sm font-medium text-rose-600 bg-rose-100 border py-2 px-4 rounded"><MdErrorOutline size={20} /> {paymentInputError}</p>}
-
-                <AdditionalPaymentInputList/>
+              {/* plus btn  */}
+              <div
+                onClick={() => { setAddSupplier(addSupplier + 1) }}
+                style={{
+                  boxShadow: "-1px 3px 8px 0px rgba(0, 0, 0, 0.2)",
+                }}
+                className="w-8 h-8 rounded-full shadow-2xl flex justify-center items-center absolute right-[50%]  translate-x-1/2 -translate-y-1/2 bg-white"
+              >
+                <button className="text-[#8633FF] hover:text-[#6519cf] transition-all duration-100">
+                  <AiOutlinePlusCircle size={24} />
+                </button>
               </div>
-            </div>
 
-            {/* plus btn  */}
-            <div
-              onClick={handleAddSupplierIncrementField}
-              style={{
-                boxShadow: "-1px 3px 8px 0px rgba(0, 0, 0, 0.2)",
-              }}
-              className="w-8 h-8 rounded-full shadow-2xl flex justify-center items-center absolute right-[50%]  translate-x-1/2 -translate-y-1/2 bg-white"
-            >
-              <button className="text-[#8633FF] hover:text-[#6519cf] transition-all duration-100">
-                <AiOutlinePlusCircle size={24} />
+              {/* delete btn  */}
+              <button
+                onClick={() => handleAddSupplierRemoveField(index)}
+                className="text-slate-400 hover:text-slate-500 transition-all duration-100 hover:cursor-pointer absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-white"
+              >
+                <AiOutlineCloseCircle size={24} />
               </button>
             </div>
-
-            {/* delete btn  */}
-            <button
-              onClick={() => handleAddSupplierRemoveField(index)}
-              className="text-slate-400 hover:text-slate-500 transition-all duration-100 hover:cursor-pointer absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-white"
-            >
-              <AiOutlineCloseCircle size={24} />
-            </button>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       {/* next btn  */}
       <button onClick={handleNext} className="flex items-center justify-center border border-[#8633FF]  w-80 mx-auto mt-12 py-[10px] rounded-md text-[#8633FF] hover:bg-[#8633FF] hover:text-white transition font-medium">
         <p>Next</p>
