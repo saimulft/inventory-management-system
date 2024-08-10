e367df790de9aacf3e973bad96c9baad27c7ae05
NabilNewaz <nabilnewaz@gmail.com>
2024-08-10 17:43:10 +0600

    updated

--- server/src/routes/payment_api.js
+++ server/src/routes/payment_api.js
@@ -4,7 +4,7 @@ const connectDatabase = require('../config/connectDatabase')
 const { addMonths } = require('date-fns');
 
 const stripe = require('stripe')('sk_test_51M9AWiKa3gcPGhKTJaOAKnhWZgT7pA0dqch2kQ7uz7M5lXpt6dDckLjHdFIyYs1rgafA64sKm7eGH05O6aWWlo52006GcXLLcN');
-const YOUR_DOMAIN = 'https://ims.nabilnewaz.com';
+const YOUR_DOMAIN = 'https://inventories.nabilnewaz.com';
 
 const sendEmail = require("../utilities/send_email");
 const verifyJWT = require("../middlewares/verifyJWT");
